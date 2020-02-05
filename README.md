cc4a7f3899e1baf7be911f89d99ed53f3a8601ed
Tracy Marshall <tracythedev@gmail.com>
2020-02-05 04:06:18 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning Java. Using the course located here: https://testautomationu.applitools.com/java-programming-course/
---
+++ java_course/src/chapter2/MadLibs.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class MadLibs {
+    public static void main(String args[]) {
+        // Ask for a season
+        System.out.println("Please enter a season of the year");
+        Scanner scanner = new Scanner(System.in);
+        var season = scanner.next();
+
+        // Ask for an adjective
+        System.out.println("Please enter an adjective");
+        var adjective = scanner.next();
+
+        // Ask for a whole number
+        System.out.println("Please enter a whole number");
+        int whNumber = scanner.nextInt();
+        scanner.close();
+
+        // Print out a string that uses the entered information
+        System.out.println("On a " + adjective + " " + season + " day, I drink a minimum of " + whNumber + " cups of coffee.");
+    }
+}
