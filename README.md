a03d8a32a9f40259d40ea991f803f80c5470645c
Tracy Marshall <tracythedev@gmail.com>
2020-02-11 14:58:41 -0500

    finished ch. 8

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Chapter files at: https://github.com/angiejones/java-programming
 To do:
 Ch. 4 Roll the Die game
 Ch. 5 Phone Bill Calculator
+Ch. 6 Phone Bill Calculator #2
+Ch. 7 Day of the Week
+Ch. 8 Password Validator
+Comment on the code to explain new things I understand
---
+++ java_course/src/chapter8/TextProcessor.java
@@ -0,0 +1,57 @@
+package chapter8;
+
+public class TextProcessor {
+
+    public static void main(String[] args) {
+        countWords("I love Test Automation University");
+        reverseString("Hello TAU!");
+        addSpaces("Hello,World!ThisIsTracy");
+    }
+
+    /**
+     * Splits a String into an array by tokenizing it
+     * Counts the contained words and prints them out
+     * @param text input string to be counted
+     */
+    public static void countWords(String text) {
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+        String message = String.format("Your text contains %d words:", numberOfWords);
+        System.out.println(message);
+
+        for (int i = 0; i < numberOfWords; i++) {
+            System.out.println(words[i]);
+        }
+    }
+
+    /**
+     * Reverses and prints the input string
+     * @param text input string to be reversed
+     */
+    public static void reverseString(String text) {
+        for (int i = text.length() - 1; i >= 0; i--) {
+            System.out.print(text.charAt(i));
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * Adds spaces before each uppercase letter in a string
+     * @param text has jumbled spaceless text
+     */
+    public static void addSpaces(String text) {
+
+        var modifiedText = new StringBuilder(text);
+
+        for (int i = 0; i < modifiedText.length(); i++) {
+            if(i != 0 && Character.isUpperCase(modifiedText.charAt(i))) {
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+
+        System.out.println(modifiedText);
+    }
+}
